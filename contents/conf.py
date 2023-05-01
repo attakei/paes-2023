2c024bd3b76750cf6d77d3ff3c1037f0dba66a7a
Kazuya Takei <myself@attakei.net>
2023-05-01 15:40:09 +0900

    feat: First adding OpenGraph

--- contents/conf.py
+++ contents/conf.py
@@ -10,6 +10,7 @@ release = "2023.5.1"
 extensions = [
     "extensions.lazy_footnotes",
     "extensions.rebuild_pageurl",
+    "sphinxext.opengraph",
 ]
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -23,3 +24,7 @@ html_static_path = ["_static"]
 html_css_files = ["css/site.css"]
 
 # -- Options for extensions
+# sphinxext-opengraph
+ogp_site_url = html_baseurl
+ogp_description_length = 100
+ogp_type = "article"
--- contents/index.rst
+++ contents/index.rst
@@ -1,3 +1,5 @@
+:og:type: website
+
 ====
 Home
 ====
