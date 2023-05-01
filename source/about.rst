79a89fbfafd2b76f045ffd57b35ab352cc1faa3f
Kazuya Takei <myself@attakei.net>
2023-05-01 14:47:18 +0900

    feat: Simple using piccolo-theme
    - Cosutomize behavior
    - Add terms.rst

---
+++ source/about.rst
@@ -0,0 +1,61 @@
+=====
+About
+=====
+
+本人について
+============
+
+基本情報
+--------
+
+:ハンドル: @attakei
+:名前: Kazuya Takei
+:職業: ソフトウェアエンジニア
+
+外部サイト
+----------
+
+`Twitter <https://twitter.com/attakei>`_
+/
+`GitHub <https://github.com/attakei>`_
+/
+`GitLab <https://gitlab.com/attakei>`_
+/
+`Zenn <https://zenn.dev/attakei>`_
+/
+`Qiita <https://qiita.com/attakei>`_
+/
+`Speaker Deck <https://speakerdeck.com/attakei>`_
+
+資格類
+------
+
+* 情報処理技術者試験
+
+  * 初級システムアドミニストレータ試験
+  * 基本情報技術者試験
+  * ソフトウェア開発技術者試験
+  * データベーススペシャリスト試験
+  * 情報セキュリティスペシャリスト試験
+
+コア領域
+--------
+
+キーワードベース
+
+* ジョブワークフロー
+* ライブラリ
+* プラグイン
+* CLIベース
+
+サイトについて
+==============
+
+``@attakei`` 個人視点での技術・趣味に関するノート・日記や、
+個人開発しているソフトウェアの解説などをまとめています。
+また、各イベントなどで発表したHTMLスライドも併せて保存しています。
+
+その他
+======
+
+* Google Analyticsによるアクセス状況の取得を行っています
--- source/conf.py
+++ source/conf.py
@@ -1,7 +1,10 @@
 """Configuration of Sphinx."""
+from sphinx.application import Sphinx
+from sphinx.util.docutils import nodes
+
 # -- Project information
 project = "attakei pages"
-copyright = "2023, Kazuya Takei"
+copyright = "2012-2023, Kazuya Takei"
 author = "Kazuya Takei"
 release = "2023.5.1"
 
@@ -12,5 +15,42 @@ exclude_patterns = []
 language = "ja"
 
 # -- Options for HTML output
-html_theme = "alabaster"
+html_theme = "piccolo_theme"
+html_title = "attakei pages"
+html_baseurl = "https://attakei.net"  # Not need to change by environment
 html_static_path = ["_static"]
+html_css_files = ["css/site.css"]
+
+# -- Options for extensions
+
+
+def collect_footnotes(app: Sphinx, doctree: nodes.document):
+    """Display all footnotes into tail of pages."""
+    footnotes = nodes.section()
+    for footnote in doctree.traverse(nodes.footnote):
+        footnote.parent.remove(footnote)
+        footnotes.append(footnote)
+    if len(footnotes.children):
+        footnotes.insert(0, nodes.rubric(text="※脚注"))
+        doctree.append(footnotes)
+
+
+def rebuild_pageurl(
+    app: Sphinx,
+    pagename: str,
+    templatename: str,
+    context: dict,
+    doctree: nodes.document,
+):
+    """Update canonical URL for dirhtml.
+
+    :ref: https://github.com/sphinx-doc/sphinx/issues/9730
+    """
+    if not app.config.html_baseurl:
+        return
+    context["pageurl"] = f"{app.config.html_baseurl}/{pagename}/"
+
+
+def setup(app: Sphinx):  # noqa: D103
+    app.connect("doctree-read", collect_footnotes)
+    app.connect("html-page-context", rebuild_pageurl)
