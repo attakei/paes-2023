3710bf9d64222179d4471fc1af2d85e71b0aadb7
Kazuya Takei <myself@attakei.net>
2023-05-01 10:53:50 +0900

    feat: Initial commit
    - Define minimal dependencies
    - Add meta files
    - Add dotfiles

---
+++ README.md
@@ -0,0 +1,1 @@
+# attakei pages
---
+++ source/conf.py
@@ -0,0 +1,16 @@
+"""Configuration of Sphinx."""
+# -- Project information
+project = "attakei pages"
+copyright = "2023, Kazuya Takei"
+author = "Kazuya Takei"
+release = "2023.5.1"
+
+# -- General configuration
+extensions = []
+templates_path = ["_templates"]
+exclude_patterns = []
+language = "ja"
+
+# -- Options for HTML output
+html_theme = "alabaster"
+html_static_path = ["_static"]
