f9b722f4f871da26d4a912471d852e845e2fee89
Kazuya Takei <myself@attakei.net>
2023-05-02 21:28:29 +0900

    feat(og-image): Write text into iamge

--- src/og_image/image.py
+++ src/og_image/image.py
@@ -4,7 +4,9 @@ from pathlib import Path
 from typing import Tuple
 
 import cairosvg
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+
+from . import spec
 
 
 def init_image(base_image_path: Path) -> Tuple[Image.Image, Path]:
@@ -13,3 +15,11 @@ def init_image(base_image_path: Path) -> Tuple[Image.Image, Path]:
     cairosvg.svg2png(url=str(base_image_path), write_to=str(img_path))
     img = Image.open(img_path)
     return img, img_path
+
+
+def write_text(img: Image.Image, spec: spec.TextSpec) -> Image.Image:
+    """Write text content for spec into target image."""
+    font = ImageFont.truetype(str(spec.font_path), spec.font_size)
+    draw = ImageDraw.Draw(img)
+    draw.text(spec.pos.to_tuple(), spec.content, (0, 0, 0), font)
+    return img
