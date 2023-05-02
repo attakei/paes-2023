871559b152fae40bb2dba2c4c0fa56a129313f19
Kazuya Takei <myself@attakei.net>
2023-05-03 06:17:13 +0900

    feat(ogp): Generating works only html-like builders

--- src/extensions/article_og_image.py
+++ src/extensions/article_og_image.py
@@ -1,23 +1,38 @@
 """Article hooks for ``og_image`` private package."""
 from copy import deepcopy
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 from docutils import nodes
-from PIL import Image
+from PIL.Image import Image
 from sphinx import addnodes
 from sphinx.application import Sphinx
-from sphinx.config import Config
 
 from og_image import image, spec
 
-_image_base: Image.Image
-_spec = spec.Spec
+_image_base: Optional[Image] = None
+_image_targets = {}
+_spec: Optional[spec.TextSpec] = None
 
 
-def append_og_image(app: Sphinx, doctree: addnodes.document):
-    """Append og_image directive if not exists."""
+def init_shared_values(app: Sphinx):
+    """Set base image and spec for wrting images.
+
+    It works when inited builder is only html-like.
+    """
+    if app.builder.format != "html":
+        return
     global _image_base, _spec
+    spec_ = spec.load_toml(Path(app.config.x_aog_image_spec), work_dir=Path.cwd())
+    _image_base, _ = image.init_image(spec_.base.path)
+    image.paste_rounted_image(_image_base, spec_.icon)
+    image.write_text(_image_base, spec_.texts[0])
+    _spec = spec_.texts[1]
+
+
+def catch_og_image_target(app: Sphinx, doctree: addnodes.document):
+    """Append og_image directive if not exists."""
+    global _image_targets
     docname = app.env.docname
     metadata = app.env.metadata[docname]
     if docname in app.config.x_aog_excludes:
@@ -26,28 +41,38 @@ def append_og_image(app: Sphinx, doctree: addnodes.document):
         image_name = docname.replace("/", "~") + f".{app.config.x_aog_format}"
         metadata["og:image"] = f"{app.config.x_aog_basepath}/{image_name}"
         out = f"{app.srcdir}/{app.config.x_aog_basepath}/{image_name}"
-        img = _image_base.copy()
-
-        text_spec = deepcopy(_spec.texts[1])
-        text_spec.content = list(doctree.traverse(nodes.title))[0].astext()
-        image.write_text(img, text_spec)
-        img.save(out)
+        text = list(doctree.traverse(nodes.title))[0].astext()
+        _image_targets[docname] = (text, out)
 
 
-def init_shared_vals(app: Sphinx, config: Config):
-    """Initialze module-wide variables."""
-    global _image_base, _spec
-    _spec = spec.load_toml(Path(config.x_aog_image_spec), work_dir=Path.cwd())
-    _image_base, _ = image.init_image(_spec.base.path)
-    image.paste_rounted_image(_image_base, _spec.icon)
-    image.write_text(_image_base, _spec.texts[0])
+def write_og_image(
+    app: Sphinx,
+    pagename: str,
+    templatename: str,
+    context: dict,
+    doctree: addnodes.document,
+):
+    """Write og-image included page title."""
+    if not doctree:
+        return
+    global _image_base, _image_targets, _spec
+    if not _image_base or not _spec:
+        return
+    if pagename in app.config.x_aog_excludes:
+        return
+    text, out = _image_targets[pagename]
+    img = _image_base.copy()
+    text_spec = deepcopy(_spec)
+    text_spec.content = text
+    image.write_text(img, text_spec)
+    img.save(out)
 
 
 def setup(app: Sphinx):  # noqa: D103
-    global _image_base, _spec
     app.add_config_value("x_aog_format", "png", "env", [str])
     app.add_config_value("x_aog_basepath", "_static", "env", [str])
     app.add_config_value("x_aog_image_spec", None, "env", [Path])
     app.add_config_value("x_aog_excludes", [], "env", [List[str]])
-    app.connect("config-inited", init_shared_vals)
-    app.connect("doctree-read", append_og_image)
+    app.connect("builder-inited", init_shared_values)
+    app.connect("doctree-read", catch_og_image_target)
+    app.connect("html-page-context", write_og_image)
