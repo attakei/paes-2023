841168ea1efb27068160d5d021cf32a2e02245b0
Kazuya Takei <myself@attakei.net>
2023-05-02 19:53:28 +0900

    feat(og-image): Define endpoint

---
+++ src/og_image/__main__.py
@@ -0,0 +1,32 @@
+# noqa: D100
+import logging
+from pathlib import Path
+
+import click
+
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option("--debug", type=bool, is_flag=True)
+@click.argument(
+    "src",
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, resolve_path=True, path_type=Path
+    ),
+)
+@click.argument(
+    "out",
+    type=click.Path(file_okay=True, dir_okay=False, resolve_path=True, path_type=Path),
+)
+def main(src: Path, out: Path, debug: bool = False):
+    """Generate of-image by spec."""
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    logger.debug("Start")
+    work_dir = Path.cwd()
+    logger.debug(f"Working-directory is {work_dir}")
+    logger.debug("End")
+
+
+main()
